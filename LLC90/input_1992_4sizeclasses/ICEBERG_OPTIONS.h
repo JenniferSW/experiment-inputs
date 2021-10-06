4d722580b6417116af393b79bc8598171a3c19e5
JenniferSW <jwesselbaum@marum.de>
2021-10-06 17:18:50 +0200

    add files from k18 server

---
+++ LLC90/input_1992_4sizeclasses/ICEBERG_OPTIONS.h
@@ -0,0 +1,43 @@
+C CPP options file for ICEBERG
+C Use this file for selecting options within package "Iceberg"
+
+#ifndef ICEBERG_OPTIONS_H
+#define ICEBERG_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS.h"
+
+#ifdef ALLOW_ICEBERG
+C Place CPP define/undef flag here
+#define ALLOW_ICEBERG_DYNAMICS
+#define ALLOW_ICEBERG_MELTING
+#define ALLOW_ICEBERG_CALVING
+#define ALLOW_FRESHWATER_INPUT
+
+C CPP flags for the use of forces in dynamics
+#define ALLOW_ICEBERG_CORIOLIS
+#define ALLOW_ICEBERG_OCEAN_DRAG
+#define ALLOW_ICEBERG_ATMOSPHERE_DRAG
+#define ALLOW_ICEBERG_SEAICE_DRAG
+#define ALLOW_ICEBERG_WAVE_RADIATION
+#define ALLOW_ICEBERG_PRESSURE_GRADIENT
+
+#define ALLOW_FIXING
+
+C to reduce memory storage, disable unused array with those CPP flags :
+#define ICEBERG_3D_STATE
+#define ICEBERG_2D_STATE
+#define ICEBERG_TENDENCY
+
+#define ICEBERG_USE_CLASS_LOGIC
+
+
+#undef MYPA_SPECIAL_COMPILE_OPTION1
+
+#define MYPA_SPECIAL_COMPILE_OPTION2
+
+#endif /* ALLOW_ICEBERG */
+#endif /* ICEBERG_OPTIONS_H */
+
+CEH3 ;;; Local Variables: ***
+CEH3 ;;; mode:fortran ***
+CEH3 ;;; End: ***
